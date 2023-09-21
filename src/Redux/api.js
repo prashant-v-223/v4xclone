2f49c2ac4e3912cac1b1fd333543536dd78b57d5
prashant-v-223 <prashantvadhvana@gmail.com>
2023-09-22 01:33:01 +0530

    asd

--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,5 +1,4 @@
-// const baseURL = process.env.REACT_APP_API_URL;
-const baseURL = "http://localhost:8080/";
+const baseURL = process.env.REACT_APP_API_URL;
 export const apiList = {
   Signup: baseURL + "api/registration/signUp",
   Signin: baseURL + "api/registration/signIn",
