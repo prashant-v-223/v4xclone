e442b0da9a2b58da4d39e1d5509ae3f99b0d8eae
prashant-v-223 <prashantvadhvana@gmail.com>
2023-09-27 01:46:30 +0530

    ad

--- src/Redux/api.js
+++ src/Redux/api.js
@@ -1,4 +1,4 @@
-const baseURL = process.env.REACT_APP_API_URL;
+const baseURL = "https://healthy-jade-headscarf.cyclic.cloud/";
 export const apiList = {
   Signup: baseURL + "api/registration/signUp",
   Signin: baseURL + "api/registration/signIn",
